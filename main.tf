8afd0a5fca3169e125fce5a8ef343de8eecc7341
Anwar <anwar_nagi@hotmail.com>
2026-06-13 21:38:49 +0100

    update

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
+resource "aws_s3_bucket" "terraformbuckettoday12345" {
+  bucket = "terraformbuckettoday12345"
+}
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraformbuckettoday12345"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
+  }
+}
